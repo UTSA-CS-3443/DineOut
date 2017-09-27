dbbca07315d37ebed4fdce76835709c8d367b614
Dylan J Horwath <jaroslavhorwath@hotmail.com>
2017-09-26 19:31:22 -0500

    Created JavaFX project and classes brought up during the meeting (might change slightly later on)

---
+++ src/application/Person.java
@@ -0,0 +1,10 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Person {
+	private String szName;
+	private String szPhoneNum;
+	private String szEmail;
+	private ArrayList<Integer> answersM = new ArrayList<Integer>();
+}
