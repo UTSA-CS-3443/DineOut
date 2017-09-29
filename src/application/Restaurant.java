09b60529c7c251e1f40c8c4b555e73ef569008f2
Mark Pena <markpena373@gmail.com>
2017-09-29 10:22:12 -0500

    Added Restaurant.java vars, methods

--- src/application/Restaurant.java
+++ src/application/Restaurant.java
@@ -1,5 +1,57 @@
 package application;
 
 public class Restaurant {
-
+	
+	// Restraunts name
+	private String name;
+	
+	// General price range
+	private int price;
+	
+	// Rating
+	private double rating;
+	
+	// Type of cuisine
+	private String cuisine;
+	
+	// Website of restaurant
+	private String website;
+	
+	// Address of restaurant
+	private String address;
+	
+	// Boolean var which says if restraunt was selected.
+	private boolean selected;
+	
+	public Restaurant(
+			String name, int price, double rating, String cuisine, 
+			String website, String address) {
+		this.name = name;
+		this.price = price;
+		this.rating = rating;
+		this.cuisine = cuisine;
+		this.website = website;
+		this.address = address;
+		this.selected = false;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSelected(boolean b) {
+		this.selected = b;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/application/StartSession.java
+++ src/application/StartSession.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.ArrayList;
+
 public class StartSession {
+	
+	ArrayList<Person> users = new ArrayList<Person>();
+	
 
 }
