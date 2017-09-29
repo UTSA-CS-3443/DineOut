68872ba78f1f3a72ec98001b2cc91645f5a8eba3
Dylan J Horwath <jaroslavhorwath@hotmail.com>
2017-09-29 10:04:52 -0500

    Added .fxml files

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,58 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	public Stage stage;
+	public AnchorPane layout;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
+//		try {
+//			this.stage = primaryStage;
+//			this.stage.setTitle("DineOut");
+//			initLayout();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+			// load up fx file
+			
+			//set fxml file location
+			
+			//show
+		
+		try {	
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,800,600);
+			Scene scene = new Scene(root,1000,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} 
+		catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+//	public void initLayout() {
+//		try {
+//			FXMLLoader loader = new FXMLLoader();
+//			loader.setLocation( Main.class.getResource("application/SBUserInterface.fxml"));
+//			this.layout = (AnchorPane) loader.load();
+//			
+//			Scene scene = new Scene( layout );
+//			this.stage.setScene( scene );
+//			this.stage.show();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
 	public static void main(String[] args) {
 		launch(args);
