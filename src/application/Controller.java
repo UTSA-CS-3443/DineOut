a1454854ffbe6ef7ee6603710a2b470fad210128
Mark <Mark@DESKTOP-A73DLTQ>
2017-10-02 01:27:53 -0500

    Forgot to add controller class to previous commit

---
+++ src/application/Controller.java
@@ -0,0 +1,127 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class Controller {
+
+    @FXML
+    private Label dineOutTitleLabel;
+
+    @FXML
+    private Label friendsInvitedLabel;
+
+    @FXML
+    private Label user1Label;
+
+    @FXML
+    private Label user2Label;
+
+    @FXML
+    private Label user3Label;
+
+    @FXML
+    private Label user4Label;
+
+    @FXML
+    private Label user5Label;
+
+    @FXML
+    private Label user6Label;
+
+    @FXML
+    private Label user7Label;
+
+    @FXML
+    private Label user8Label;
+
+    @FXML
+    private Label user9Label;
+
+    @FXML
+    private Label user10Label;
+
+    @FXML
+    private Pane user1LabelBackground;
+
+    @FXML
+    private Pane user3LabelBackground;
+
+    @FXML
+    private Pane user4LabelBackground;
+
+    @FXML
+    private Pane user5LabelBackground;
+
+    @FXML
+    private Pane user6LabelBackground;
+
+    @FXML
+    private Pane user7LabelBackground;
+
+    @FXML
+    private Pane user8LabelBackground;
+
+    @FXML
+    private Pane user9LabelBackground;
+
+    @FXML
+    private Pane user10LabelBackground;
+
+    @FXML
+    private Pane user1IndicatorBackground;
+
+    @FXML
+    private Pane user2IndicatorBackground;
+
+    @FXML
+    private Pane user3IndicatorBackground;
+
+    @FXML
+    private Pane user4IndicatorBackground;
+
+    @FXML
+    private Pane user5IndicatorBackground;
+
+    @FXML
+    private Pane user6IndicatorBackground;
+
+    @FXML
+    private Pane user7IndicatorBackground;
+
+    @FXML
+    private Pane user8IndicatorBackground;
+
+    @FXML
+    private Pane user9IndicatorBackground;
+
+    @FXML
+    private Pane user10IndicatorBackground;
+
+    @FXML
+    private Pane user2LabelBackground;
+
+    @FXML
+    private Button previousRestaurantButton;
+
+    @FXML
+    private Label restaurantNumberLabel;
+
+    @FXML
+    private Button nextRestaurantButton;
+
+    @FXML
+    private Label restaurantNameLabel;
+
+    @FXML
+    private Button websiteButton;
+
+    @FXML
+    private Label cuisineTypeLabel;
+
+    @FXML
+    private Label priceLabel;
+
+}
