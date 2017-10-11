9fe3ca2eec422bbe2d5cccaf58d5649e1fd432ff
Brandon Lwowski <brandonlwowski@Brandons-MacBook-Pro.local>
2017-10-10 21:16:46 -0500

    Good Night Commit

--- src/application/Person.java
+++ src/application/Person.java
@@ -1,9 +1,13 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -49,7 +53,7 @@ public class Person {
 		}
 		reader.close();
 	}
-
+	
 	public String getSzName() {
 		return szName;
 	}
