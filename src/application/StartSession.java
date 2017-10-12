70042df066a678844ed07480ebe8bf197bd4192c
Mark Pena <markpena373@gmail.com>
2017-10-12 17:40:04 -0500

    Changed fillRandomList() method in StartSession

--- src/application/StartSession.java
+++ src/application/StartSession.java
@@ -19,19 +19,35 @@ public class StartSession {
 		RestaurantArray restaurants = new RestaurantArray();
 		Random rand = new Random();
 		int randomNumber = rand.nextInt(restaurants.getRestaurantList().size() + 1);
+
+		for(int count = 0 ; count < 5 ; count++) {
+			int i = randomNumber;
+			if(restaurants.getRestaurantList().get(i).isSelected() == true) {
+				continue;
+			}else { // meaning its false
+				fiveChoices.add(restaurants.getRestaurantList().get(i));
+				restaurants.getRestaurantList().get(i).setSelected(true);
+			}
+		}
+		
+		
+		/*
+		Random rand = new Random();
+		int randomNumber = rand.nextInt(restaurants.getRestaurantList().size() + 1);
 		for( int i = 0 ; i < 5 ; i++) {
 			int j = randomNumber;
-			for(Restaurant r : fiveChoices) {
+			//for(Restaurant r : fiveChoices) {
 				// if j is in fiveChoices, add k instead
-				if(r == restaurants.getRestaurantList().get(j)) { 
+				if(restaurants.getRestaurantList().get(j).isSelected() == true) { 
 					int k = randomNumber;
 					fiveChoices.add(restaurants.getRestaurantList().get(k));
 				// if j isn't in fiveChoices, add j
-				}else {
+				}else{
 					fiveChoices.add(restaurants.getRestaurantList().get(j));
 				}
 			}
-		}	
+		}
+		*/	
 	}
 	
 	
