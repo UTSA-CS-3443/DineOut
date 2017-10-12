f886d7f3908352d0989426903dcb51a3eae6129e
Mark Pena <markpena373@gmail.com>
2017-10-12 16:41:43 -0500

    Added method to StartSession to select 5 random restaurants

--- src/application/StartSession.java
+++ src/application/StartSession.java
@@ -6,52 +6,33 @@ import java.util.Random;
 public class StartSession {
 	
 	ArrayList<Restaurant> fiveChoices = new ArrayList<Restaurant>();
-	//ArrayList<Restaurant> fiveChoices = new ArrayList<Restaurant>();
 	ArrayList<Person> users = new ArrayList<Person>();
-	RestaurantArray restaurants = new RestaurantArray();
 	
-	public  void returnArrayList(){
-		/*
+	public StartSession() {
+		fillRandomList();
+	}
+	
+	/**
+	 * Fills fiveChoices arraylist with 5 "random" restaurants
+	 */
+	public void fillRandomList(){
+		RestaurantArray restaurants = new RestaurantArray();
 		Random rand = new Random();
 		int randomNumber = rand.nextInt(restaurants.getRestaurantList().size() + 1);
 		for( int i = 0 ; i < 5 ; i++) {
 			int j = randomNumber;
-			int k = randomNumber;
 			for(Restaurant r : fiveChoices) {
-				if(r == restaurants.getRestaurantList().get(j) && r != restaurants.getRestaurantList().get(k)) {
+				// if j is in fiveChoices, add k instead
+				if(r == restaurants.getRestaurantList().get(j)) { 
+					int k = randomNumber;
 					fiveChoices.add(restaurants.getRestaurantList().get(k));
-				}else if( (r != restaurants.getRestaurantList().get(j) && r != restaurants.getRestaurantList().get(k)) && (j != k)) {
+				// if j isn't in fiveChoices, add j
+				}else {
 					fiveChoices.add(restaurants.getRestaurantList().get(j));
-					fiveChoices.add(restaurants.getRestaurantList().get(k));
-				}else if( r != restaurants.getRestaurantList().get(j) && r == restaurants.getRestaurantList().get(k)) {
-					fiveChoices.add(restaurants.getRestaurantList().get(j));
-				}else if( r == restaurants.getRestaurantList().get(j) && j == k) {
-					
-				}
-				else {
-					fiveChoices.add(pullNumbersFromHere.get(j));
 				}
 			}
-		}
-		*/
-		//return numbers;
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		}	
 	}
+	
+	
 }
