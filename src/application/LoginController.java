115a1122ec9a35399724262c8d8c516e8082b863
Alberto Jasso <betojasso@hotmail.com>
2017-10-12 21:40:22 -0500

    Finalized login fxml and controller. Need to implement methods.

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,5 +1,32 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
 
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    private TextField twitterHandleTextField;
+
+    @FXML
+    private Button addUserButton;
+
+    @FXML
+    private Button doneWithScene;
+
+    @FXML
+    public void addPerson(ActionEvent e) {
+    		Person p = new Person(userNameTextField.getText());
+    		//add person to session or somwhere...
+    }
+    
+    @FXML
+    public void done(ActionEvent e) {
+    		//do something
+    }
 }
